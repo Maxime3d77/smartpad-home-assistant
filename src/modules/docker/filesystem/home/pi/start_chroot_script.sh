a1d9e9259c278f833fe45fd2471dc12c845782ca
Maxime3d77 <114477279+Maxime3d77@users.noreply.github.com>
2024-06-18 21:13:46 +0200

    script chroot

---
+++ src/modules/docker/filesystem/home/pi/start_chroot_script.sh
@@ -0,0 +1,65 @@
+#!/usr/bin/env bash
+# CustomPiOS module : docker
+# Original script written by Damien DALY (https://github.com/MaitreDede/)
+# Changes by Guy Sheffer
+# GPL V3
+########
+# shellcheck enable=require-variable-braces
+# Source error handling, leave this in place
+set -ex
+
+# Source CustomPIOS common.sh
+# shellcheck disable=SC1091
+source /common.sh
+install_cleanup_trap
+
+echo_green "Install Docker IO ..."
+apt-get update
+apt-get install -y docker.io
+echo_green "Install Docker IO ...(DONE)"
+
+echo_green "Add user Docker ..."
+if [ "${DOCKER_ADD_USER_TO_GROUP}" == "yes" ]; then
+    usermod "${BASE_USER}" -aG docker
+fi
+echo_green "Add user Docker ...(DONE)"
+
+if [ "${DOCKER_COMPOSE}" == "yes" ]; then
+    echo_green "Install python..."
+    apt-get install -y python3 python3-distutils python3-dev python3-testresources gcc libffi-dev build-essential libssl-dev cargo python3-cryptography python3-bcrypt python3-pip
+    echo_green "Install python...(DONE)"
+    
+    # Upgrade pip to the latest version
+    pip3 install --upgrade pip
+    
+    # Install PyYAML ignoring pre-installed versions
+    echo_green "Install PyYAML ..."
+    apt-get install -y python3-yaml
+    echo_green "Install PyYAML ...(DONE)"
+    
+    # Install docker-compose
+    echo_green "Install Docker compose ..."
+    apt-get install -y docker-compose
+    echo_green "Install Docker compose ...(DONE)"
+fi
+
+echo_green "Unpack Docker file & service..."
+if [ "${DOCKER_COMPOSE_BOOT}" == "yes" ]; then
+    unpack /filesystem/root /
+    unpack /filesystem/boot /"${BASE_BOOT_MOUNT_PATH}"
+    
+    if [ "${DOCKER_COMPOSE_BOOT_PATH}" == "default" ]; then
+        DOCKER_COMPOSE_BOOT_PATH_ACTUAL="/${BASE_BOOT_MOUNT_PATH}"/docker-compose
+    else
+        DOCKER_COMPOSE_BOOT_PATH_ACTUAL="${DOCKER_COMPOSE_BOOT_PATH}"
+    fi
+    sed -i "s@DOCKER_COMPOSE_BOOT_PATH_PLACEHOLDER@${DOCKER_COMPOSE_BOOT_PATH_ACTUAL}@g" /etc/systemd/system/docker-compose.service
+    sed -i "s@DOCKER_COMPOSE_BOOT_PATH_PLACEHOLDER@${DOCKER_COMPOSE_BOOT_PATH_ACTUAL}@g" /usr/bin/start_docker_compose
+    sed -i "s@DOCKER_COMPOSE_BOOT_PATH_PLACEHOLDER@${DOCKER_COMPOSE_BOOT_PATH_ACTUAL}@g" /usr/bin/stop_docker_compose
+    systemctl enable docker-compose.service
+fi
+echo_green "Unpack Docker file & service...(DONE)"
+
+#cleanup
+apt-get clean
+apt-get autoremove -y
