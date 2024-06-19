c139d07f114ca93a633229a5b6480cfb53ca3c13
Maxime3d77 <114477279+Maxime3d77@users.noreply.github.com>
2024-06-19 06:44:40 +0200

    Update start_chroot_script.sh

--- src/modules/docker/filesystem/home/pi/start_chroot_script.sh
+++ src/modules/docker/filesystem/home/pi/start_chroot_script.sh
@@ -19,45 +19,41 @@ apt-get install -y docker.io
 echo_green "Install Docker IO ...(DONE)"
 
 echo_green "Add user Docker ..."
-if [ "${DOCKER_ADD_USER_TO_GROUP}" == "yes" ]; then
-    usermod "${BASE_USER}" -aG docker
-fi
+usermod "${BASE_USER}" -aG docker
 echo_green "Add user Docker ...(DONE)"
 
-if [ "${DOCKER_COMPOSE}" == "yes" ]; then
-    echo_green "Install python..."
-    apt-get install -y python3 python3-distutils python3-dev python3-testresources gcc libffi-dev build-essential libssl-dev cargo python3-cryptography python3-bcrypt python3-pip
-    echo_green "Install python...(DONE)"
+
+echo_green "Install python..."
+apt-get install -y python3 python3-distutils python3-dev python3-testresources gcc libffi-dev build-essential libssl-dev cargo python3-cryptography python3-bcrypt python3-pip
+echo_green "Install python...(DONE)"
     
-    # Upgrade pip to the latest version
-    pip3 install --upgrade pip
+# Upgrade pip to the latest version
+pip3 install --upgrade pip
     
-    # Install PyYAML ignoring pre-installed versions
-    echo_green "Install PyYAML ..."
-    apt-get install -y python3-yaml
-    echo_green "Install PyYAML ...(DONE)"
+# Install PyYAML ignoring pre-installed versions
+echo_green "Install PyYAML ..."
+apt-get install -y python3-yaml
+echo_green "Install PyYAML ...(DONE)"
     
-    # Install docker-compose
-    echo_green "Install Docker compose ..."
-    apt-get install -y docker-compose
-    echo_green "Install Docker compose ...(DONE)"
-fi
+# Install docker-compose
+echo_green "Install Docker compose ..."
+apt-get install -y docker-compose
+echo_green "Install Docker compose ...(DONE)"
+
 
 echo_green "Unpack Docker file & service..."
-if [ "${DOCKER_COMPOSE_BOOT}" == "yes" ]; then
-    unpack /filesystem/root /
-    unpack /filesystem/boot /"${BASE_BOOT_MOUNT_PATH}"
+unpack /filesystem/root /
+unpack /filesystem/boot /"${BASE_BOOT_MOUNT_PATH}"
     
-    if [ "${DOCKER_COMPOSE_BOOT_PATH}" == "default" ]; then
-        DOCKER_COMPOSE_BOOT_PATH_ACTUAL="/${BASE_BOOT_MOUNT_PATH}"/docker-compose
-    else
-        DOCKER_COMPOSE_BOOT_PATH_ACTUAL="${DOCKER_COMPOSE_BOOT_PATH}"
-    fi
-    sed -i "s@DOCKER_COMPOSE_BOOT_PATH_PLACEHOLDER@${DOCKER_COMPOSE_BOOT_PATH_ACTUAL}@g" /etc/systemd/system/docker-compose.service
-    sed -i "s@DOCKER_COMPOSE_BOOT_PATH_PLACEHOLDER@${DOCKER_COMPOSE_BOOT_PATH_ACTUAL}@g" /usr/bin/start_docker_compose
-    sed -i "s@DOCKER_COMPOSE_BOOT_PATH_PLACEHOLDER@${DOCKER_COMPOSE_BOOT_PATH_ACTUAL}@g" /usr/bin/stop_docker_compose
-    systemctl enable docker-compose.service
+if [ "${DOCKER_COMPOSE_BOOT_PATH}" == "default" ]; then
+    DOCKER_COMPOSE_BOOT_PATH_ACTUAL="/${BASE_BOOT_MOUNT_PATH}"/docker-compose
+else
+    DOCKER_COMPOSE_BOOT_PATH_ACTUAL="${DOCKER_COMPOSE_BOOT_PATH}"
 fi
+sed -i "s@DOCKER_COMPOSE_BOOT_PATH_PLACEHOLDER@${DOCKER_COMPOSE_BOOT_PATH_ACTUAL}@g" /etc/systemd/system/docker-compose.service
+sed -i "s@DOCKER_COMPOSE_BOOT_PATH_PLACEHOLDER@${DOCKER_COMPOSE_BOOT_PATH_ACTUAL}@g" /usr/bin/start_docker_compose
+sed -i "s@DOCKER_COMPOSE_BOOT_PATH_PLACEHOLDER@${DOCKER_COMPOSE_BOOT_PATH_ACTUAL}@g" /usr/bin/stop_docker_compose
+systemctl enable docker-compose.service
 echo_green "Unpack Docker file & service...(DONE)"
 
 #cleanup
